c1bf4bda512e01917bb070cc9dc235c722b50671
lrodenyoder <rodenlau000@gmail.com>
2022-02-14 16:28:06 -0600

    9.2.7

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+
 //console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    //this...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+// const printProfileData = profileDataArr => {
+//     //this...
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
 
-    console.log("=====================")
+//     console.log("=====================")
 
-    //is the same as this....
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
+//     //is the same as this....
+//     profileDataArr.forEach((profileItem) => {
+//         console.log(profileItem)
+//     });
 
-    console.log("=====================");
+//     console.log("=====================");
 
-    //and this
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//     //and this
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs)
+// printProfileData(profileDataArgs)
+
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log("Portfolio complete! Checkout index.html to see output")
+});
