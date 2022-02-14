b4fe48b64ffac981fbdc048c4a4f0d0b83f9440f
lrodenyoder <rodenlau000@gmail.com>
2022-02-14 14:11:34 -0600

    9.1.6

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Hello node!");
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+//console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    //this...
+    for (let i = 0; i < profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log("=====================")
+
+    //is the same as this....
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileItem)
+    });
+
+    console.log("=====================");
+
+    //and this
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs)
